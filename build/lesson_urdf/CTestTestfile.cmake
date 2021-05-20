8b5b13d44da1092873eaa8e570c461f394e7283d
Clement Truillet <clement@ctruillet.eu>
2021-05-20 16:34:48 +0200

    update gitignore

--- build/lesson_urdf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ctruillet/lesson_urdf
-# Build directory: /home/ctruillet/lesson_urdf/build/lesson_urdf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/youbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ctruillet/youbot_description
-# Build directory: /home/ctruillet/youbot_description/build/youbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
