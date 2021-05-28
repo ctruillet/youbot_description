71bc03ca880b6524045d2c5685c76584cb3dd42a
Clement Truillet <clement@ctruillet.eu>
2021-05-28 15:20:07 +0200

    add video youbot_move_arm.mp4

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -4,19 +4,20 @@ from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
-       return LaunchDescription([
-           ExecuteProcess(
-               cmd=['gazebo', '--verbose', 'sdf/world.sdf', '-s libgazebo_ros_factory.so'],
-               output='screen'
-           ),
-           Node(
-               package='teleop_twist_keyboard',
-               namespace='teleop_twist_keyboard',
-               executable='teleop_twist_keyboard',
-               remappings=[
-                   ('/teleop_twist_keyboard/cmd_vel','/youbot/cmd_vel')
-               ],
-               output='screen',
-               prefix = 'xterm -e'
-           )
-   ])
+        return LaunchDescription([
+            ExecuteProcess(
+            #    cmd=['gazebo', '--verbose', 'sdf/world.sdf', '-s libgazebo_ros_factory.so'],
+                cmd=['gazebo', '--verbose', 'sdf/world.sdf'],
+                output='screen'
+            ),
+            Node(
+                package='teleop_twist_keyboard',
+                namespace='teleop_twist_keyboard',
+                executable='teleop_twist_keyboard',
+                remappings=[
+                    ('/teleop_twist_keyboard/cmd_vel','/youbot/cmd_vel')
+                ],
+                output='screen',
+                prefix = 'xterm -e'
+            )
+    ])
